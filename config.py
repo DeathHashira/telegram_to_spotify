9869a344915a8c695affd2052a603ae3b7abaa51
Zahra Sha'bani <shabanid.zahra@gmail.com>
2025-07-04 14:22:48 +0330

    config file

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+
+redirect_uri = 'https://localhost:8888/callback'
