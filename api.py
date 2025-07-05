95c6c65a6a8ef0154753bdf0bedcfb23c7b8b239
Zahra Sha'bani <shabanid.zahra@gmail.com>
2025-07-05 20:09:35 +0330

    add PKCE authorization method and scopes

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from config import *
-import requests, urllib.parse, webbrowser, base64
+import requests, urllib.parse, webbrowser, base64, random, string
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from hashlib import sha256
 
 class AuthHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -8,7 +9,6 @@ class AuthHandler(BaseHTTPRequestHandler):
         params = urllib.parse.parse_qs(query)
         code = params.get('code')
         if code:
-            print("Authorization code received:", code[0])
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b'Authorization successful. You can close this tab.')
@@ -18,11 +18,27 @@ class AuthHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'Missing authorization code.')
 
-def request_user_athurization():
+def code_verifier(num):
+    possibility = string.ascii_letters + string.digits
+    code = ''.join(random.choice(possibility) for _ in range(num))
+
+    return code
+
+def code_challenge():
+    code = code_verifier(64)
+    hashed = sha256(code.encode('utf-8')).digest()
+    encoded = base64.urlsafe_b64encode(hashed).rstrip(b'=')
+
+    return encoded.decode('utf-8'), code
+
+def request_user_athurization(code_cha):
     query = {
         'client_id':client_id,
         'response_type':'code',
-        'redirect_uri':redirect_uri
+        'redirect_uri':redirect_uri,
+        'code_challenge_method':'S256',
+        'code_challenge':code_cha,
+        'scope':'playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public'
     }
 
     url = f'{base_url}/authorize?' + urllib.parse.urlencode(query)
@@ -34,18 +50,19 @@ def get_code():
     server.handle_request()
     return server.code
 
-def get_access_token():
-    request_user_athurization()
+def get_access_token(codecha, codever):
+    request_user_athurization(codecha)
     auth_code = get_code()
 
     body = {
         'grant_type':'authorization_code',
         'code':auth_code,
-        'redirect_uri':redirect_uri
+        'redirect_uri':redirect_uri,
+        'client_id':client_id,
+        'code_verifier':codever
     }
 
     header = {
-        'Authorization':f'Basic {base64.b64encode(f'{client_id}:{client_secret}'.encode()).decode()}',
         'Content-Type':'application/x-www-form-urlencoded'
     }
 
@@ -54,5 +71,6 @@ def get_access_token():
 
     return res.json().get('access_token')
 
-print(get_access_token())
+my_challenge, my_verified = code_challenge()
+print(get_access_token(my_challenge, my_verified))
 
