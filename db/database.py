627acde35a13c1738cb6853c842c2e046af74f65
Zahra Sha'bani <shabanid.zahra@gmail.com>
2025-07-15 08:46:19 +0330

    modify database

--- db/database.py
+++ db/database.py
@@ -10,11 +10,11 @@ def close_connection(conn:sqlite3.Connection):
     conn.close()
 
 def add_new_user(conn:sqlite3.Connection ,cursor:sqlite3.Cursor, email, password):
-    cursor.execute('INSERT INTO users (name, passw) VALUES (?, ?)', (email, password))
+    cursor.execute('INSERT INTO users (email, passw) VALUES (?, ?)', (email, password))
     conn.commit()
 
 def add_tokens(conn:sqlite3.Connection ,cursor:sqlite3.Cursor, access_token, refresh_token, email):
-    cursor.execute('UPDATE users SET access_token = ?, refresh_tokne = ? WHERE email = ?', (access_token, refresh_token, email))
+    cursor.execute('UPDATE users SET access_token = ?, refresh_token = ? WHERE email = ?', (access_token, refresh_token, email))
     conn.commit()
 
 def add_user_id(conn:sqlite3.Connection ,cursor:sqlite3.Cursor, email, user_id):
@@ -24,3 +24,61 @@ def add_user_id(conn:sqlite3.Connection ,cursor:sqlite3.Cursor, email, user_id):
 def add_playlist(conn:sqlite3.Connection ,cursor:sqlite3.Cursor, user_id, playlist_id, playlist_name):
     cursor.execute('INSERT INTO playlists (user_id, playlist_id, playlist_name) VALUES (?, ?, ?)', (user_id, playlist_id, playlist_name))
     conn.commit()
+
+def is_user_id(cursor:sqlite3.Cursor, email):
+    cursor.execute('''
+        SELECT CASE
+                WHEN user_id IS NULL OR user_id = '' THEN 1
+                ELSE 0
+            END AS is_empty
+        FROM users
+        WHERE email = ?
+    ''', (email,))
+    
+    result = cursor.fetchone()
+
+    return result[0] if result else None
+
+def playlists(cursor:sqlite3.Cursor, email):
+    cursor.execute('''
+        SELECT playlists.playlist_name
+        FROM users
+        JOIN playlists ON playlists.user_id = users.user_id
+        WHERE users.email = ?
+    ''', (email,))
+
+    return cursor.fetchall()
+
+def is_pass_correct(cursor:sqlite3.Cursor, email, passw):
+    cursor.execute('''
+        SELECT CASE
+                WHEN EXISTS (
+                    SELECT 1
+                    FROM users
+                    WHERE email = ? AND passw = ?
+                ) THEN 1
+                ELSE 0
+            END
+    ''', (email, passw))
+
+    return cursor.fetchone()[0]
+
+def is_user_there(cursor:sqlite3.Cursor, email):
+    cursor.execute('''
+        SELECT CASE
+                WHEN EXISTS (
+                    SELECT 1 FROM users WHERE email = ?
+                ) THEN 1
+                ELSE 0
+            END
+    ''', (email,))
+
+    return cursor.fetchone()[0]
+
+def get_tokens(cursor:sqlite3.Cursor, email):
+    cursor.execute('''
+        SELECT access_token, refresh_token FROM users WHERE email = ?
+    ''', (email,))
+
+    result = cursor.fetchone()
+    return result[0], result[1]
