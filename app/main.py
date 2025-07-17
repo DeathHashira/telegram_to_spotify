a7fee8bffd0e4a419598f33cf1fd94c76a713775
Zahra Sha'bani <shabanid.zahra@gmail.com>
2025-07-17 16:35:43 +0330

    modify database schema and add icon

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from gui.gui import *
-import sys
+from db.schema import run
+import sys, os
+
+if not os.path.exists('spotify.db'):
+    run()
 
 def load_stylesheet(file_path):
     with open(file_path, "r") as f:
--- db/schema.py
+++ db/schema.py
@@ -1,26 +1,27 @@
 import sqlite3
 
-conn = sqlite3.connect('spotify.db')
-cursor = conn.cursor()
+def run():
+    conn = sqlite3.connect('spotify.db')
+    cursor = conn.cursor()
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL,
-    passw TEXT NOT NULL,
-    user_id TEXT,
-    access_token TEXT,
-    refresh_token TEXT
-)
-''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT NOT NULL,
+        passw TEXT NOT NULL,
+        user_id TEXT,
+        access_token TEXT,
+        refresh_token TEXT
+    )
+    ''')
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS playlists (
-    user_id TEXT NOT NULL,
-    playlist_id TEXT NOT NULL,
-    playlist_name TEXT NOT NULL
-)
-''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS playlists (
+        user_id TEXT NOT NULL,
+        playlist_id TEXT NOT NULL,
+        playlist_name TEXT NOT NULL
+    )
+    ''')
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
